58e8bcb042c9517142af3a4ba289dde8e4f3a2c1
DomDom <dominicfisher182001@gmail.com>
2023-09-05 13:08:06 -0400

    finished initial styling of product page, made mobile responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,6 @@ menuButton.addEventListener('click', () => {
         .classList.add('onscroll')
 })
 
-document.querySelector('.see-deals')
-    .addEventListener('click', () => {
-        location.href = "#clearance"
-    })
-
-document.querySelector('.js-view-all')
-    .addEventListener('click', productPage)
-document.querySelector('.js-view-all-2')
-    .addEventListener('click', productPage)
-
-function productPage() {
-    location.href = "/outdoor-expeditions/product-page.html"
-}
-
 document.onscroll = () => {
     tableOfContents.classList.add('hidden')
     menuButton.classList.remove('rotate')
