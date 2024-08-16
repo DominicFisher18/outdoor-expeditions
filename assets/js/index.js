203ef48373ee804da37419f5da35492c0da9bcbf
DomDom <dominicfisher182001@gmail.com>
2024-08-16 19:22:11 -0400

    added new js file, going to generate html to mske code neater

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,23 @@
 
 
-function productPage() {
-    location.href = "/outdoor-expeditions/product-page.html"
-    console.log('hello')
-    event.preventDefault()
-}
+// function productPage() {
+//     location.href = "/outdoor-expeditions/product-page.html"
+//     console.log('hello')
+//     event.preventDefault()
+// }
 
-function changePage() {
-    location.href = "/outdoor-expeditions/product-page.html";
-    event.preventDefault()
-}
+// function changePage() {
+//     location.href = "/outdoor-expeditions/product-page.html";
+//     event.preventDefault()
+// }
 
-document.querySelector('.js-view-all')
-    .addEventListener('click', productPage)
-document.querySelector('.js-view-all-2')
-    .addEventListener('click', productPage)
+// function navToCart() {
+//     location.href = "/outdoor-expeditions/cart-page.html"
+// }
+
+// document.querySelector('.js-view-all')
+//     .addEventListener('click', productPage)
+// document.querySelector('.js-view-all-2')
+//     .addEventListener('click', productPage)
 
  
