1c7ad906afbf7467cb6d4176db7429cadccd576e
DomDom <dominicfisher182001@gmail.com>
2024-08-18 18:26:06 -0400

    created products data strcuture, generating HTML rather than writing it

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,3 +1,126 @@
-const products = [
-    
-]
+const products = [{
+    image: '/outdoor-expeditions/assets/images/fishing-pole.jpg',
+    name: 'Black Fishing Pole',
+    priceDollars: 140,
+    category: 'All',
+    subcategory: 'Fishing'
+}, {
+    image: '/outdoor-expeditions/assets/images/fishing-hat.jpg',
+    name: 'Fishing Hat',
+    priceDollars: 25,
+    category: 'All',
+    subcategory: 'Fishing'
+}, {
+    image: '/outdoor-expeditions/assets/images/tackle-box.jpg',
+    name: 'Tackle Box',
+    priceDollars: 35,
+    category: 'All',
+    subcategory: 'Fishing'
+}, {
+    image: '/outdoor-expeditions/assets/images/tent.png',
+    name: 'Light Blue Tent',
+    priceDollars: 30,
+    category: 'All',
+    subcategory: 'Camping'
+}, {
+    image: '/outdoor-expeditions/assets/images/hammock.png',
+    name: 'Colorful Hammock',
+    priceDollars: 20,
+    category: 'All',
+    subcategory: 'Camping'
+}, {
+    image: '/outdoor-expeditions/assets/images/green-canoe.png',
+    name: 'Green Canoe',
+    priceDollars: 650,
+    category: 'All',
+    subcategory: 'Camping'
+}, {
+    image: '/outdoor-expeditions/assets/images/firewood.jpg',
+    name: 'Firewood',
+    priceDollars: 12,
+    category: 'All',
+    subcategory: 'Camping'
+}, {
+    image: '/outdoor-expeditions/assets/images/sleeping-bag.jpg',
+    name: 'Sleeping Bag',
+    priceDollars: 22,
+    category: 'All',
+    subcategory: 'Camping'
+}, {
+    image: '/outdoor-expeditions/assets/images/trekking-pole.jpg',
+    name: 'Trekking Pole',
+    priceDollars: 80,
+    category: 'All',
+    subcategory: 'Hiking'
+}, {
+    image: '/outdoor-expeditions/assets/images/hiking-boots.jpg',
+    name: 'Hiking Boots',
+    priceDollars: 55,
+    category: 'All',
+    subcategory: 'Hiking'
+}, {
+    image: '/outdoor-expeditions/assets/images/hiking-backpack.jpg',
+    name: 'Hiking Backpack',
+    priceDollars: 120,
+    category: 'All',
+    subcategory: 'Hiking'
+}, {
+    image: '/outdoor-expeditions/assets/images/hiking-headlamp.jpg',
+    name: 'Hiking Headlamp',
+    priceDollars: 20,
+    category: 'All',
+    subcategory: 'Hiking'
+}, {
+    image: '/outdoor-expeditions/assets/images/round-grill.png',
+    name: 'Black Round Grill',
+    priceDollars: 110,
+    category: 'All',
+    subcategory: 'Backyard'
+}, {
+    image: '/outdoor-expeditions/assets/images/fence.jpg',
+    name: 'Custom Fence',
+    priceDollars: 15,
+    category: 'All',
+    subcategory: 'Backyard'
+}, {
+    image: '/outdoor-expeditions/assets/images/spatula.jpg',
+    name: 'Steel Spatula',
+    priceDollars: 30,
+    category: 'All',
+    subcategory: 'Backyard'
+}, {
+    image: '/outdoor-expeditions/assets/images/birdfeeder.jpg',
+    name: 'Birdfeeder',
+    priceDollars: 18,
+    category: 'All',
+    subcategory: 'Backyard'
+}, {
+    image: '/outdoor-expeditions/assets/images/trail-mix.jpg',
+    name: 'Trail Mix',
+    priceDollars: 12,
+    category: 'All',
+    subcategory: 'Food'
+}, {
+    image: '/outdoor-expeditions/assets/images/beef-jerky.jpeg',
+    name: 'Beef Jerky',
+    priceDollars: 18,
+    category: 'All',
+    subcategory: 'Food'
+}]
+
+let productsHTML = ''
+
+products.forEach((product) => {
+    productsHTML += `<div class="product-box">
+                    <div class="image">
+                        <img src="${product.image}">
+                    </div>
+                    <div class="product-description">
+                        <h4 class="item-name">${product.name}</h4>
+                        <p class="item-price">$${product.priceDollars}</p>
+                    </div>
+                </div>
+                `
+})
+
+document.querySelector('.product-js-container').innerHTML = productsHTML
